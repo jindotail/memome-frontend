1f15a597a76eb7eec061470366c3ad8ab1a52d71
Sumin <sumin.kim.dev@gmail.com>
2022-05-28 21:52:47 +0900

    Feat: 전송 버튼 클릭 시 말풍선 추가 기능

--- src/components/utils/Comments.js
+++ src/components/utils/Comments.js
@@ -1,10 +1,10 @@
 import styles from "./Comments.module.css";
 
-function Comments() {
+function Comments({ comment }) {
     return (
         <div className={styles.container}>
-            <div className={styles.comment}>안녕하세요~!</div>
-            <div className={styles.date}>2022-05-26</div>
+            <div className={styles.comment}>{comment.comment}</div>
+            <div className={styles.date}>{comment.date}</div>
         </div>
     )
 }
--- src/pages/Guestbook.js
+++ src/pages/Guestbook.js
@@ -1,8 +1,13 @@
+import { useRef } from 'react';
+import { useNavigate } from "react-router";
+import useFetch from "../hooks/useFetch";
 import Comments from '../components/utils/Comments';
 import styles from "./Guestbook.module.css";
 
 function Guestbook() {
     const url = window.location.href;
+    const comments = useFetch("http://localhost:3001/comment");
+    const navigate = useNavigate();
 
     // 방명록 주소 복사 함수
     const handleCopy = async (text) => {
@@ -13,20 +18,46 @@ function Guestbook() {
             alert('복사 실패!');
         }
     };
+
+    // 전송 버튼 함수
+    function onSubmit(e) {
+        e.preventDefault();
+        console.log(commentRef.current.value);
+
+        fetch(`http://localhost:3001/comment`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                comment: commentRef.current.value,
+                date: "xxxx-xx-xx"
+            })
+        }).then(res => {
+            if (res.ok) {
+                alert("전송이 되었습니다.");
+                window.location.replace("/")
+
+            }
+        })
+    }
+
+    const commentRef = useRef(null);
+
     return (
         <div className={styles.guestbook}>
             <span className={styles.title}> Jindo's Guestbook</span>
             <div className={styles.container}>
                 <div className={styles.contents}>
-                    <Comments />
-                    <Comments />
+                    {comments.map(comment => (
+                        <Comments comment={comment} key={comment.id} />
+                    ))}
                 </div>
-                <div className={styles.inputBox}>
-                    <input />
+                <form className={styles.inputBox} onSubmit={onSubmit}>
+                    <input type="text" ref={commentRef} />
                     <button className={styles.submitButton}>전송</button>
-                </div>
+                </form>
             </div>
-            {console.log(url)}
             <button className={styles.shareButton} onClick={() => handleCopy(url)}>방명록 공유하기</button>
         </div >
 
