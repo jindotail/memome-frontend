1768a45b50a5dee2e0b9566df99d2773fbe6bea7
Sumin <sumin.kim.dev@gmail.com>
2022-06-26 22:32:32 +0900

    Feat: login api 연동

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
+import { useRef } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Main from '../components/utils/Main';
 import styles from "./Login.module.css";
 
 function Login() {
+    // 가입하기 버튼 함수
+    function onSubmit(e) {
+        e.preventDefault();
+
+        axios.post(`http://localhost:8080/api/auth/login`,
+            {
+                id: idRef.current.value,
+                password: passwordRef.current.value,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    withCredentials: true
+                }
+
+            }
+        )
+            .then(res => {
+                console.log("로그인 성공");
+                window.location.replace((`/${idRef.current.value}`));
+            })
+            .catch(res => { console.log('Error!') });
+    };
+
+    const idRef = useRef(null);
+    const passwordRef = useRef(null);
+
     return (
         <Main>
             <section className={styles.loginPart}>
@@ -11,12 +40,12 @@ function Login() {
                         Guest Book
                     </div>
                 </div>
-                <form action="" method="POST" className={styles.fromStyle}>
+                <form action="" method="POST" className={styles.fromStyle} onSubmit={onSubmit}>
                     <div className={styles.inputBox}>
-                        <input id="username" type="text" name="username" placeholder="아이디" className={styles.login} />
+                        <input id="username" type="text" name="username" placeholder="아이디" className={styles.login} ref={idRef} />
                     </div>
                     <div className={styles.inputBox}>
-                        <input id="password" type="password" name="password" placeholder="비밀번호" className={styles.login} />
+                        <input id="password" type="password" name="password" placeholder="비밀번호" className={styles.login} ref={passwordRef} />
                     </div>
                     <Link to="/signup" className={styles.signIn}>회원가입 하기</Link>
                     <button type="submit" className={styles.submitButton}>Log In</button>
