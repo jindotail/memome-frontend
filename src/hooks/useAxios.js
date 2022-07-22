5cd041fe4096d7db3285e707784e664c945beb09
Sumin <sumin.kim.dev@gmail.com>
2022-07-22 11:05:12 +0900

    Fix: 존재하지 않는 유저의 방명록 접근 방지

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,10 +1,12 @@
 // 실시간으로 comment 추가되는 기능 유지시 삭제 예정 파일
 
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
 export default function useAxios(url) {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(url)
@@ -14,7 +16,11 @@ export default function useAxios(url) {
             .then(data => {
                 setData(data);
             })
-            .catch(error => { console.log(Error) });
+            .catch(error => {
+                console.log(Error);
+                alert("존재하지 않는 방명록입니다.")
+                navigate('/');
+            });
     }, [url]);
 
     return data;
