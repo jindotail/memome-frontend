773f5f8bba48f59824c8848313125efa9e8fcdfd
Sumin <sumin.kim.dev@gmail.com>
2022-11-04 18:19:24 +0900

    Feat: ReadMe 페이지 개발 완료

--- src/pages/Readme.js
+++ src/pages/Readme.js
@@ -1,30 +1,68 @@
+import { useRef, useState } from 'react';
+import Menu from '../components/utils/Menu';
+import { getCookie } from '../hooks/cookie';
 import styles from "./Readme.module.css";
 
-function Readme () {
+function Readme () {  
+    const userId = getCookie("user_id");
+
+    const idRef = useRef(null);
+
+    const [id, setId] = useState("memome");
+
+    const onSubmit = () => {
+      setId(idRef.current.value);
+    }
+
     return (
       <div>
-        <header className={styles.header}>Memome ReadMe</header>
+        <header className={styles.header}>
+          Memome ReadMe
+          {/* {(document.cookie.length > 0)?(  
+            <div className={styles.menu}>
+              <Menu user={userId} />
+            </div>
+          ):null} */}
+        </header>
         <section className={styles.container}>
           <div className={styles.main}>
             <div>
-              <p>Markdown</p>
+              <p className={styles.title}>ID 입력</p>
+              <div>
+                <input className={styles.input} placeholder="유저 ID를 입력하세요" ref={idRef}/>
+                <button className={styles.idButton} onClick={onSubmit}>입력</button>
+              </div>
+            </div> 
+            <br />
+            <div>
+              <p className={styles.title}>Markdown</p>
               <div className={styles.markdown}>
                 <p>
-                  {/* 마크다운 코드 보여줌 */}{" "}
-                  [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter)](https://hits.seeyoufarm.com){" "}
+                  {/* 마크다운 코드 보여줌 */}
+                  [![Memome Profile](https://memome-readme.herokuapp.com/v1/layout?id={id})](https://memome.be/{id})
                 </p>
-                <button className={styles.copyButton}>COPY!</button>
+                <div className={styles.copyButtonContainer}>
+                  <button className={styles.copyButton}>복사</button>
+                </div>
               </div>
             </div>
+            <br />
+            <br />
             <div>
-              <p>사용 방법</p>
-              <div>{/* 이미지 및 설명 방법  */}</div>
-            </div>
-            <div>
-              <p>Demo</p>
-              <div>{/* Version 1  */}</div>
-              <div>{/* Version 2  */}</div>
-            </div>
+              <p className={styles.title}>Demo</p>
+              <div className={styles.demoContainer}>
+                <div className={styles.demo}>
+                  <p className={styles.demoTitle}>{id}의 방명록</p>
+                  <div className={styles.demoSubTitle}>Recent comments</div>
+                  <div className={styles.comments}>안녕하세요</div>
+                  <div className={styles.comments}>{id}의 방명록입니다~!</div>
+                  <div className={styles.logoContainer}>
+                    <div className={styles.logo}>MEMOME</div>
+                  </div>                
+                </div>
+                <div>{/* Version 2  */}</div>  
+              </div>
+             </div> 
           </div>
         </section>
       </div>
