e54b5f6a2ef2d08d3d935dcb7bce157dc40fd3c3
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-02-08 14:44:09 +0900

    Chore: 회원탈퇴 여부 재질문 팝업창 설정

--- src/components/utils/Withdrawal.js
+++ src/components/utils/Withdrawal.js
@@ -5,23 +5,31 @@ import { removeCookie } from '../../hooks/cookie';
 import { token } from '../../hooks/token';
 
 export function withdrawal(user) {
-    axios.delete(`${process.env.REACT_APP_API_URL}/api/user/${user}`)
-        .then(res => {
+    const result = confirm("정말 회원탈퇴를 하시겠습니까?");
+
+    if(result) {
+        axios
+          .delete(`${process.env.REACT_APP_API_URL}/api/user/${user}`)
+          .then((res) => {
             // handle success
             console.log(res);
             removeCookie("user_id");
             window.location.replace(`/`);
-            alert("탈퇴되었습니다")
-        })
-        .catch(error => {
-            const userInfo = { user }
+            alert("탈퇴되었습니다");
+          })
+          .catch((error) => {
+            const userInfo = { user };
             // handle error
             if (error.response.status === 401) {
-                console.log("토큰이 만료되었습니다");
-                token(userInfo, "회원탈퇴");
-                //withdrawal(user);
+              console.log("토큰이 만료되었습니다");
+              token(userInfo, "회원탈퇴");
+              //withdrawal(user);
             } else {
-                console.log(error);
-            };
-        })
+              console.log(error);
+            }
+          });
+    } else {
+        return;
+    }
+    
 }
