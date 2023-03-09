ad7e0d4b753c39ffe7ed789220c2cc01632cce51
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-03-09 18:24:19 +0900

    Feat: store 추가

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
 import axios from "axios";
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.withCredentials = true;
 root.render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </React.StrictMode>
+    <Provider store={store}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </Provider>
 );
 
 
